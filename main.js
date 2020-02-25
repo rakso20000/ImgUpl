f490bd1e7e2892921c0012339a2c5cb705d4ebfe
rakso20000 <rakso20000@gmail.com>
2020-02-25 12:42:30 +0100

    Route requests

--- main.js
+++ main.js
@@ -1,8 +1,28 @@
 const http = require('http');
+const url = require('url');
 
 const port = 8080;
 
+const destinations = {};
+
+destinations.upload = async (req, res) => {
+	
+};
+
 const handleRequest = async (req, res) => {
+	
+	const parsedUrl = url.parse(req.url);
+	const pathName = parsedUrl.pathname;
+	
+	if (destinations.hasOwnProperty(pathName)) {
+		
+		await destinations[pathName](req, res);
+		
+	} else {
+		
+		
+		
+	}
 	
 };
 
