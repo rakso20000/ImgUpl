085f8e1af4f543a6b7726d81f6608ff0685f6194
rakso20000 <rakso20000@gmail.com>
2020-02-25 15:05:27 +0100

    Sanitize inputs

--- main.js
+++ main.js
@@ -7,6 +7,40 @@ const destinations = {};
 
 destinations.upload = async (req, res) => {
 	
+	if (req.method !== 'POST') {
+		
+		res.statusCode = 405;
+		res.setHeader('Content-type', 'text/plain');
+		res.end('405 - Method Not Allowed');
+		
+		return;
+		
+	}
+	
+};
+
+const view = async (req, res, code) => {
+	
+	if (req.method !== 'GET') {
+		
+		res.statusCode = 405;
+		res.setHeader('Content-type', 'text/plain');
+		res.end('405 - Method Not Allowed');
+		
+		return;
+		
+	}
+	
+	if (!code.match(/^[0-9a-zA-Z]+$/)) {
+		
+		res.statusCode = 400;
+		res.setHeader('Content-type', 'text/plain');
+		res.end('400 - Bad Request');
+		
+		return;
+		
+	}
+	
 };
 
 const handleRequest = async (req, res) => {
@@ -20,7 +54,7 @@ const handleRequest = async (req, res) => {
 		
 	} else {
 		
-		
+		await view(req, res, pathName);
 		
 	}
 	
